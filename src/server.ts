ee45d244b547613e142625fa63245d3770969bbc
vishak <vishakvenu1998@gmail.com>
2023-06-24 23:42:29 +0530

    fixes

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://food-app-backend-vevc.onrender.com/');
+  res.header('Access-Control-Allow-Origin', 'https://food-app-backend-vevc.onrender.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
