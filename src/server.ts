be2dd08b82f11774925a38a6eaceb3118f24d7b2
vishak <vishakvenu1998@gmail.com>
2023-06-24 23:40:00 +0530

    swagger fixes

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,12 @@ const app: Express = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://food-app-backend-vevc.onrender.com/');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 connectDB().then(()=>{
   app.listen(port, () => {
