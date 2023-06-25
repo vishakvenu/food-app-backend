a0b07837ca01a296ce74350b2071add13ca7c8fd
vishak <vishakvenu1998@gmail.com>
2023-06-25 08:34:59 +0530

    swagger fixes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,6 +4,47 @@ import { registerUser } from '../controllers/userController';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     description: Register a new user with the provided name, number, email, and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               number:
+ *                 type: number
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - name
+ *               - number
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: User registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object 
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Email is already registered
+ *       500:
+ *         description: Server Error
+ */
 router.route("/register").post(registerUser)
 
 export default router;
