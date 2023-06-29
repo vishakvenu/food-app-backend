fd308be67ed34e853efe71a3af5450c9886a4f3b
vishak <vishakvenu1998@gmail.com>
2023-06-29 23:08:53 +0530

    build final changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/node-express-cicd/deploy.log
-
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/node-express-cicd/deploy.log
-cd /home/ec2-user/node-express-cicd >> /home/ec2-user/node-express-cicd/deploy.log
-
-echo 'npm install' >> /home/ec2-user/node-express-cicd/deploy.log 
-npm install >> /home/ec2-user/node-express-cicd/deploy.log
+set -e
+cd /usr/share/nginx/html
+npm install
+npm run build
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16.16.0
-
-#create our working directory if it doesnt exist
-DIR="/home/ec2-user/node-codedeploy"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+set -e
+yum update -y
+pm2 update
